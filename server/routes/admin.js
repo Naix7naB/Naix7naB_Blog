a65f932fd29e77f48cf6c6f24295c5ae0f390c02
Naix7naB <duemarsgh@foxmail.com>
2022-11-29 22:54:48 +0800

    feat: 新增登录/注册接口

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const User = require('../models/User')
 const assert = require('http-assert')
 const createError = require('http-errors')
 
 const { getPubKey } = require('../core/rsa')
+const { encrypt, decrypt, sendToken, handleError } = require('../plugins/utils')
 
 const Router = express.Router()
 
@@ -15,6 +17,26 @@ Router.get('/key', async (req, res, next) => {
     message: '获取密钥成功',
     data: { pubKey }
   })
+})
+
+// 注册
+Router.post('/register', async (req, res, next) => {
+  try {
+    req.body.password = encrypt(req.body.password)
+    const user = await User.create(req.body)
+    res.json(sendToken(user, '注册成功'))
+  } catch (err) {
+    next(createError(422, handleError(err)))
+  }
+})
+
+// 登录
+Router.post('/login', async (req, res, next) => {
+  const { username, password } = req.body
+  const user = await User.findOne({ username }).select('+password')
+  assert(user, 422, '用户不存在')
+  assert.equal(password, decrypt(user.password), 422, '账号或密码错误')
+  res.json(sendToken(user, '登录成功'))
 })
 
 module.exports = Router
