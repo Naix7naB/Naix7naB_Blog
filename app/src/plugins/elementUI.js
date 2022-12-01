f1070f4b8caca62fec2a4968032d987855968337
Naix7naB <duemarsgh@foxmail.com>
2022-12-01 20:04:47 +0800

    feat: 引入 element-ui 响应式布局

--- app/src/plugins/elementUI.js
+++ app/src/plugins/elementUI.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 
 import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
 
 // TODO 优化: 暂时全部引入, 后续应该为按需引入
 Vue.use(ElementUI)
