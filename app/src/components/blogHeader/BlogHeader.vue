b54915aa3672fd771c46f9e25e750c224c4a4cbf
Naix7naB <duemarsgh@foxmail.com>
2022-12-02 00:20:37 +0800

    feat: 修改组件文件夹存放结构, 完成 BlogHeader 组件

---
+++ app/src/components/blogHeader/BlogHeader.vue
@@ -0,0 +1,76 @@
+<script>
+import Navigator from './components/navigator/Navigator'
+
+export default {
+  name: 'BlogHeader',
+  components: { Navigator },
+  data() {
+    return {
+      routes: [
+        {
+          name: 'Home',
+          title: '主页',
+          icon: 'home'
+        },
+        {
+          name: 'Messages',
+          title: '留言板',
+          icon: 'comments'
+        },
+        {
+          name: 'Photos',
+          title: '相册',
+          icon: 'images'
+        },
+        {
+          name: 'About',
+          title: '关于',
+          icon: 'circle-info'
+        },
+        {
+          name: 'Personal',
+          title: '个人中心',
+          icon: 'user'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <header class="blog-header">
+    <el-row class="blog-header--wrap">
+      <el-col class="blog-logo" tag="h1" :span="8">
+        <a href="javascript:;">
+          <font-awesome-icon icon="fa-solid fa-blog" />
+          <span class="blog-logo--title">&#35;7x Blog</span>
+        </a>
+      </el-col>
+      <el-col :span="16">
+        <Navigator :list="routes" />
+      </el-col>
+    </el-row>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.blog-header {
+  padding: 1% 5%;
+  box-shadow: 0 0 10px #333;
+
+  .blog-header--wrap {
+    display: flex;
+    align-items: center;
+
+    .blog-logo {
+      font-size: 32px;
+
+      .blog-logo--title {
+        margin-left: 10px;
+        font-family: kanit;
+      }
+    }
+  }
+}
+</style>
---
+++ app/src/components/blogHeader/components/navigator/Navigator.vue
@@ -0,0 +1,59 @@
+<script>
+import SearchBar from './components/searchBar/SearchBar'
+
+export default {
+  name: 'Navigator',
+  components: { SearchBar },
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <nav class="blog-navigator">
+    <SearchBar />
+    <ul class="blog-nav--list hidden-xs-only">
+      <li class="blog-nav--item" v-for="item in list" :key="item.name">
+        <a href="javascript:;">
+          <font-awesome-icon :icon="['fa-solid', `fa-${item.icon}`]" />
+          <span class="blog-nav--title">{{item.title}}</span>
+        </a>
+      </li>
+    </ul>
+    <div class="hidden-sm-and-up">
+      <font-awesome-icon icon="fa-solid fa-minus" />
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.blog-navigator {
+  display: flex;
+  align-items: center;
+
+  .blog-nav--list {
+    display: flex;
+    align-items: center;
+    font-size: $fz-medium;
+
+    .blog-nav--item {
+      margin: 0 10px;
+      padding: 8px 10px;
+      border-radius: 6px;
+      transition: 0.2s ease-in;
+
+      &:hover {
+        background-color: rgba($color: #7f7f7f, $alpha: 0.3);
+      }
+
+      .blog-nav--title {
+        margin-left: 4px;
+      }
+    }
+  }
+}
+</style>
