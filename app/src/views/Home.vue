c99f93071c8ce580344c9d45f091f326eb9e705b
Naix7naB <duemarsgh@foxmail.com>
2022-12-05 22:49:27 +0800

    feat: 新增固定头部滚动时的过渡效果

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,35 +1,72 @@
-<template>
-  <div class="home">
-    <BlogHeader />
-    <BlogFooter />
-  </div>
-</template>
-
 <script>
-import { BlogHeader, BlogFooter } from '@/components'
+import { BlogHeader, BlogBanner, BlogFooter } from '@/components'
+
+// TODO 背景图片从后端请求
+import bgImage from '@/assets/images/bg.png'
 
 export default {
   name: 'Home',
   components: {
     BlogHeader,
+    BlogBanner,
     BlogFooter
+  },
+  data(){
+    return {
+      defaultBgc: bgImage,
+      maxTransY: 1200,
+      scrollTop: 0
+    }
+  },
+  computed: {
+    headerStyle() {
+      let blurCount = 0, bgColor = 'transparent', shadow = 'none'
+      let ratio = Math.min(this.scrollTop / this.maxTransY * 100, 100) / 100
+      if (this.scrollTop > 0) {
+        blurCount = Math.min(this.scrollTop / 50, 20)
+        bgColor = `rgba(100, 100, 100, ${Math.min(ratio, .3)})`
+        shadow = `0 1px 20px rgba(51, 51, 51, ${Math.min(ratio, .7)})`
+      }
+      return {
+        backgroundColor: bgColor,
+        boxShadow: shadow,
+        backdropFilter: `blur(${blurCount}px)`
+      }
+    }
+  },
+  methods:{
+    onScroll(vertical) {
+      this.scrollTop = vertical.scrollTop
+    }
   }
 }
 </script>
 
+<template>
+  <div class="home" :style="{ backgroundImage: `url(${defaultBgc})` }">
+    <VueScroll @handle-scroll="onScroll">
+      <BlogHeader :headerStyle="headerStyle" />
+      <BlogBanner />
+      <BlogFooter style="height: 10000px;" />
+    </VueScroll>
+  </div>
+</template>
+
 <style lang="scss" scoped>
+:deep(.__vuescroll .__rail-is-vertical){
+  z-index: 1000 !important;
+}
+
 .home {
-  height: 10000px;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 
   &:before {
+    @include full-fixed();
     content: '';
-    display: block;
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    @include bg-image('@/assets/images/bg');
+    background-color: rgba($color: #000, $alpha: .3);
   }
 }
 </style>
