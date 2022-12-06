bd6eccb1675cb05e847514b88f9579c236216326
Naix7naB <duemarsgh@foxmail.com>
2022-12-06 21:29:21 +0800

    feat: 新增 BlogMain 组件

---
+++ app/src/components/blogMain/BlogMain.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'BlogMain',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<template>
+  <div class="blog-main">Main</div>
+</template>
+
+<style lang="scss" scoped></style>
--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as BlogHeader } from './blogHeader/BlogHeader'
 export { default as BlogBanner } from './blogBanner/BlogBanner'
+export { default as BlogMain } from './blogMain/BlogMain'
 export { default as BlogFooter } from './blogFooter/BlogFooter'
